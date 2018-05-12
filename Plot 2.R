50b1c508f5bbd6f1692590c4607fc1972236c535
adrianromano <31232599+adrianromano@users.noreply.github.com>
2018-05-12 18:20:55 -0500

    Exploratory Data Analysis - Course Project 2

---
+++ Plot 2.R
@@ -0,0 +1,16 @@
+## Set working directory and Load Data
+setwd("/Users/adrianromano/Downloads")
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+
+## Filter dates from 2007/02/01 to 2007/02/02
+subset_data <- subset(data, Date %in% c("1/2/2007", "2/2/2007"))
+
+## Format and combine Date and Time columns
+subset_data$Date <- as.Date(subset_data$Date, format = "%d/%m/%Y")
+dateTime <- paste(as.Date(subset_data$Date), subset_data$Time)
+subset_data$dateTime <- as.POSIXct(dateTime)
+
+## Plot 2
+plot(subset_data$Global_active_power ~ subset_data$dateTime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png, "plot2.png", width = 480, height = 480)
+dev.off()
